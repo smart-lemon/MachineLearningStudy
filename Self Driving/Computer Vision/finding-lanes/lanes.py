38df4c9a001ba37c50f37db0eb8db9ecd07da40c
Reno <siq.samuel@gmail.com>
2018-11-28 23:52:40 +0530

    'Self driving program - finding lanes - completed edge detection'

--- Self Driving/Computer Vision/finding-lanes/lanes.py
+++ Self Driving/Computer Vision/finding-lanes/lanes.py
@@ -1,25 +1,23 @@
 import cv2
+import numpy as np
 
 image = cv2.imread('./test_image.jpg')
 
+# Make a copy and work with it - turn it gray 
+lane_image = np.copy(image) 
+gray_scale = cv2.cvtColor(lane_image, cv2.COLOR_RGB2GRAY)
+
+# Apply Gaussian blur
+blurry_image = cv2.GaussianBlur(gray_scale, (5, 5), 0)
+
+# Note: Canny does Gaussian blur by itself
+canny_image = cv2.Canny(blurry_image, 50,  105)
+
 # Window name - result
-cv2.imshow('result', image)
+cv2.imshow('result', canny_image)
 
 # Show image and wait for key
 cv2.waitKey(0)
-
-# Make a copy and work with it - turn it gray 
-lane_image = np.copy(image) 
-gray_scale = cv2.cvtColor(lane_image. cv2.COLOR_RGB2GRAY)
-
-# Apply Gaussian blur
-blurry_iamge = cv2.GaussianBlur(gray_scale, (5, 5), 0)
-
-# Note: Canny does Gaussian blur by itself
-
-
-
-
 
 
 # Prerequistes
