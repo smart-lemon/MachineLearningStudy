90931768e0872f502c0a6203356e16647e95df2e
Reno <siq.samuel@gmail.com>
2018-12-02 19:50:17 +0530

    'Self driving program - finding lanes - Functional implmentation for images'

--- Self Driving/Computer Vision/finding-lanes/lanes.py
+++ Self Driving/Computer Vision/finding-lanes/lanes.py
@@ -37,7 +37,16 @@ def display_lines(test_img, lines):
             cv2.line( line_image, (x1, y1), (x2, y2),
                       (25, 255, 0) )
     return line_image
-                  
+
+def display_lines_avg(test_img, lines):
+    line_image = np.zeros_like(test_img)
+    if lines is not None: 
+        for line in lines: 
+            x1, y1, x2, y2 = line 
+            cv2.line( line_image, (x1, y1), (x2, y2),
+                      (25, 255, 0) )
+    return line_image
+
 def region_of_interest(test_image):
     height = test_image.shape[0]
 
@@ -51,9 +60,9 @@ def region_of_interest(test_image):
     masked_image = cv2.bitwise_and(test_image, mask)
     return masked_image
 
-def make_points(image, line):
-    slope, intercept = line
-    y1 = int(image.shape[0])   # bottom of the image
+def make_points(test_image, line_parameters):
+    slope, intercept = line_parameters
+    y1 = int(test_image.shape[0])   # bottom of the image
     y2 = int(y1 * 3/5)         # slightly lower than the middle
     x1 = int((y1 - intercept)/slope)
     x2 = int((y2 - intercept)/slope)
@@ -61,20 +70,24 @@ def make_points(image, line):
 
 
 def average_slope_intercept(image, lines):
+    # Contains intercept of line on left and right
     left_fit    = []
     right_fit   = []
     if lines is None:
         return None
     for line in lines:
         for x1, y1, x2, y2 in line:
-            fit   = np.polyfit((x1, x2), (y1, y2), 1)
+            fit = np.polyfit((x1, x2), (y1, y2), 1)
+
+            # Detemine the slope and intercept
             slope = fit[0]
             intercept = fit[1]
+
             if slope < 0: # y is reversed in image
                 left_fit.append((slope, intercept))
             else:
                 right_fit.append((slope, intercept))
-    # add more weight to longer lines
+    # Add more weight to longer lines
     left_fit_average  = np.average(left_fit, axis = 0)
     right_fit_average = np.average(right_fit, axis = 0)
     left_line  = make_points(image, left_fit_average)
@@ -83,10 +96,8 @@ def average_slope_intercept(image, lines):
     return averaged_lines
 
 def detect_lanes_in_image():
-
-
+    # Open the image and make copy
     image = cv2.imread('./test_image.jpg')
-
     lane_image = np.copy(image)
 
     # Perform canny
@@ -113,6 +124,14 @@ def detect_lanes_in_image():
     # Combine the images with the lines - the weight is multiplied by all the pixel
     combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 
                                 1) # the gamma
+    show_image_matplot(combo_image)
+
+    averaged_lines = average_slope_intercept(lane_image, lines)
+
+    # Add the average lines and mix it into the image
+    line_image = display_lines(lane_image, np.array(averaged_lines))
+    combo_image = cv2.addWeighted(lane_image, 0.8, line_image, 1, 1)
+    
     show_image_matplot(combo_image)
 
 detect_lanes_in_image()
